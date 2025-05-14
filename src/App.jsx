61581e33fc283c42e464b2460af0635d752e8744
Vivek Kumar Barnwal <vivekbarnwal2001@gmail.com>
2025-05-14 16:12:23 +0530

    Implemented ShowCase section and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import NavBar from "./components/NavBar"
 import HeroSection from "./sections/HeroSection"
+import ShowcaseSection from "./sections/ShowcaseSection"
 
 const App = () => {
   return (
     <>
+    <NavBar />
     <HeroSection />
+    <ShowcaseSection />
     </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import { navLinks } from '../constants'
+
+const NavBar = () => {
+    const [scrolled, setScrolled]  = useState(false);
+
+    useEffect(()=>{
+        const handleScroll = () => {
+            const isScrolled = window.scrollY > 10;
+            setScrolled(true);
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return() => window.removeEventListener('scroll', handleScroll);
+    }, [])
+  return (
+    <header className={`navbar ${scrolled ? 'scrolled': "not-Scrolled"}`}>
+        <div className='inner'>
+            <a className='logo' href="#hero">
+                Vivek | VKB
+            </a>
+
+            <nav className='desktop'>
+                <ul>
+                    {navLinks.map(({ link , name})=>(
+                        <li key={name} className='group'>
+                            <a href={link}>
+                                <span>{name}</span>
+                                <span className='underline' />
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+
+            <a href="#contact" className='contact-btn group'>
+                <div className='inner'>
+                    <span>Contact Me</span>
+                </div>
+            </a>
+        </div>
+    </header>
+  )
+}
+
+export default NavBar
