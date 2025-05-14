299fbcc050aa9368d9bdfa3b78a7dab4b7baa559
Vivek Kumar Barnwal <vivekbarnwal2001@gmail.com>
2025-05-14 18:50:02 +0530

    implemented LogoSection and FeatureCards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import LogoSection from "./components/LogoSection"
 import NavBar from "./components/NavBar"
+import FeatureCards from "./sections/FeatureCards"
 import HeroSection from "./sections/HeroSection"
 import ShowcaseSection from "./sections/ShowcaseSection"
 
@@ -8,6 +10,8 @@ const App = () => {
     <NavBar />
     <HeroSection />
     <ShowcaseSection />
+    <LogoSection />
+    <FeatureCards />
     </>
   )
 }
